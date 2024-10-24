9d4b5b176743cb192ed95e625647a775e3066808
Ofelia888 <acbk123@gmail.com>
2024-10-24 12:51:38 +0200

    20 sided die

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+var r = new Random();
+int dice = r.Next(20) + 1;
+
+Console.WriteLine(dice);
+
+if (dice >= 10)
+{
+    if (dice == 20)
+        Console.WriteLine("CRITICAL HIT");
+    else
+    Console.WriteLine("HIT!");
+}
+else if (dice >5)
+    Console.WriteLine("WEAK");
+else
+{
+    Console.WriteLine("MISS");
+}
